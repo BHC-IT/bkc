c8df1050732cfeb39bced0eac3467f4f873ff887
lanath <valentin.vivier@epitech.eu>
2019-02-28 11:44:41 +0100

    using weak_import in place of weak

--- bfc/bfcmain.hpp
+++ bfc/bfcmain.hpp
@@ -3,10 +3,14 @@
 #include <string>
 #include <vector>
 
+#ifndef weak_import
+	#define weak_import weak
+#endif
+
 namespace bfc {
 	/// the main function is used in the static library. if you want to use your hown main, you're inveited to link with the shared library rather than the static one.
-	// std::vector<std::string>	ReqFlags() __attribute__((weak));
-	void				initActor() __attribute__((weak));
-	int				main() __attribute__((weak)); ///< main called by blc after a clean start-up
-	void				closure() __attribute__((weak));
+	std::vector<std::string>	ReqFlags() __attribute__((weak_import));
+	void				initActor() __attribute__((weak_import));
+	int				main() __attribute__((weak_import)); ///< main called by blc after a clean start-up
+	void				closure() __attribute__((weak_import));
 }
