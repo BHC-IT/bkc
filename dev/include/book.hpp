895b750c492e1eb6ed9a31aaa39d2cb7d39cc9a0
lanath <valentin.vivier@bhc-it.com>
2019-03-05 14:30:59 +0100

    multi-proof transaction implemented

--- dev/include/book.hpp
+++ dev/include/book.hpp
@@ -24,7 +24,7 @@ namespace bkc {
 		std::vector<bkc::trans>	getByAmount(double amount) const;
 		std::vector<bkc::trans>	getByTime(int time) const;
 
-		std::vector<bkc::trans>	getAllProof(const std::string &keys);
+		std::vector<bkc::trans>	getAllProof(const std::string &keys) const;
 
 		std::string 	serialize() const;
 		void		unserialize(const std::string &str);
